4e195d07f84cb738a62137381f14c553fed119ca
タブキー <tabkeeeey@gmail.com>
2025-11-15 20:55:02 +0900

    feat(product): 2511 商品検索機能の改善(FULLTEXT)

--- webapp/mysql/migration/0_sample.sql
+++ webapp/mysql/migration/0_sample.sql
@@ -1,1 +1,10 @@
 -- このファイルに記述されたSQLコマンドが、マイグレーション時に実行されます。
+ALTER TABLE orders
+ADD INDEX idx_user_id (user_id);
+
+ALTER TABLE orders
+ADD INDEX idx_shipped_status (shipped_status);
+
+ALTER TABLE products
+ADD FULLTEXT INDEX idx_ft_name_desc (name, description)
+WITH PARSER ngram;
